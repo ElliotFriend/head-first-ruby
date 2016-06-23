ab203193a9e85a401fc2d6be6e2d2c740bb6f04b
Elliot J. Voris <elliot@voris.me>
2016-06-22 22:20:47 -0500

    Finishing up the dog class in animals.rb
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- ch02/animals.rb
+++ ch02/animals.rb
@@ -9,7 +9,21 @@ end
 
 class Dog
 
-  attr_accessor :name, :age
+  attr_reader :name, :age
+
+  def name=(value)
+    if value == ""
+      raise "Name can't be blank!"
+    end
+    @name = value
+  end
+
+  def age=(value)
+    if value < 0
+      raise "An age of #{value} isn't valid!"
+    end
+    @age = value
+  end
 
   def report_age
     puts "#{@name} is #{@age} years old."
