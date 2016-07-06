46cbbc5fa20252b3ef415078785a843ad8fb8e83
Elliot J. Voris <elliot@voris.me>
2016-07-05 21:20:11 -0500

    Adding camera_methods, from the Code Magnets puzzle
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

---
+++ ch03/camera_methods.rb
@@ -0,0 +1,18 @@
+class Camera
+
+  def load
+    puts "Winding film."
+  end
+
+  def take_picture
+    puts "Triggering shutter."
+  end
+
+end
+
+class DigitalCamera < Camera
+  def load
+    puts "Inserting memory card."
+  end
+
+end
